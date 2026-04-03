5cf591543bdfc73a4a143d863df4b6e138ad3696
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-04-03 08:15:24 +0300

    [pr02_3] Import parsers into Main.hs and test it. Lab2 done. Closes #2

--- practice2/myParser/app/Main.hs
+++ practice2/myParser/app/Main.hs
@@ -1,9 +1,23 @@
 module Main (main) where
 
+import Lib
+import qualified MyParsers.MyAttoparsec as AP
+import qualified MyParsers.MyParsec as PP
+import qualified MyParsers.MyParser as MP
 import MyTypes.MyEither
 import MyTypes.MyMaybe
 import MyTypes.MyTree
-import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "MyParser:"
+  putStrLn $ show (MP.runParser MP.plusOrMult "12*345dsf")
+  putStrLn $ show (MP.runParser MP.plusOrMult "12+345dsf")
+
+  putStrLn "Parsec:"
+  putStrLn $ show (PP.runParser PP.plusOrMultParsec "12*345dsf")
+  putStrLn $ show (PP.runParser PP.plusOrMultParsec "12+345dsf")
+
+  putStrLn "Attoparsec:"
+  putStrLn $ show (AP.runParser AP.plusOrMultAttoparsec "12*345dsf")
+  putStrLn $ show (AP.runParser AP.plusOrMultAttoparsec "12+345dsf")
