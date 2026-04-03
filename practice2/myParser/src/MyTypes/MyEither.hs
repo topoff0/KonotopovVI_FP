bfa3b33e196eddbb8b0cccbdb5096018e984013c
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-04-03 22:23:53 +0300

    [Fix] Fix MyEither Semigroup instance implementation

--- practice2/myParser/src/MyTypes/MyEither.hs
+++ practice2/myParser/src/MyTypes/MyEither.hs
@@ -6,30 +6,14 @@ instance Foldable (MyEither a) where
   foldr _ z (MyLeft _) = z
   foldr f z (MyRight y) = f y z
 
--- | @since base-4.9.0.0
--- instance Semigroup (Either a b) where
---     Left _ <> b = b
---     a      <> _ = a
-
--- Какая реализация верная: прокидываем MyLeft дальше (как ошибку) или берем первое верное значение (MyRight)?
--- instance Semigroup (MyEither a b) where
---   (<>) (MyLeft _) b = b
---   (<>) a _ = a
-
-instance (Semigroup a) => Semigroup (MyEither b a) where
-  (<>) (MyLeft x) _ = MyLeft x
-  (<>) _ (MyLeft x) = MyLeft x
-  (<>) (MyRight x) (MyRight y) = MyRight (x <> y)
+instance Semigroup (MyEither a b) where
+  (<>) (MyLeft _) b = b
+  (<>) a _ = a
 
 instance Functor (MyEither a) where
   fmap _ (MyLeft x) = MyLeft x
   fmap f (MyRight y) = MyRight (f y)
 
--- | @since base-3.0
--- instance Applicative (Either e) where
---     pure          = Right
---     Left  e <*> _ = Left e
---     Right f <*> r = fmap f r
 instance Applicative (MyEither a) where
   pure x = MyRight x
   (<*>) (MyLeft a) _ = MyLeft a
