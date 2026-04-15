797b0b5361b3841147ac3274e9ec5d5dd74aa1a9
topoff0 <vikonotopoff@gmail.com>
2026-04-15 23:14:36 +0300

    [pr03_2] Add core types for labyrinth and movement in it. Add 'mtl' package for RWS

---
+++ practice3/Labyrinth/src/Core/Types.hs
@@ -0,0 +1,13 @@
+module Core.Types(Room, Labyrinth, Log, Visited, Search) where
+
+import Control.Monad.RWS
+
+type Room = String
+
+type Labyrinth = (Room, [Room])
+
+type Log = [String]
+
+type Visited = [Room]
+
+type Search = RWS Log Labyrinth Visited
