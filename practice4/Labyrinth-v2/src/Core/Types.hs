a1cf06cd57950827d35ab074dd504266540af319
topoff0 <vikonotopoff@gmail.com>
2026-04-22 22:10:50 +0300

    [pr04_1] Implement core types

---
+++ practice4/Labyrinth-v2/src/Core/Types.hs
@@ -0,0 +1,21 @@
+module Core.Types
+  ( Room
+  , Labyrinth
+  , Log
+  , Visited
+  , Search
+  ) where
+
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Core.MyStateT
+
+type Room = String
+type Labyrinth = [(Room, [Room])]
+type Log = [String]
+type Visited = [Room]
+
+type Search m a =
+  MyStateT Visited
+    (WriterT Log
+      (ReaderT Labyrinth m)) a
