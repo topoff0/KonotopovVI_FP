47ed9bc186da34c72ef532f5d96c7a6669e318ec
topoff0 <vikonotopoff@gmail.com>
2026-05-06 20:59:31 +0300

    [pr05_1] Add reverseWords function

--- practice5/qclab/src/Lib.hs
+++ practice5/qclab/src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
   ( addMod,
+    reverseWords,
   )
 where
 
@@ -9,3 +10,14 @@ addMod x y m
   | (x + y) < m && (x + y) > 0 = x + y
   | (x + y) < 0 = addMod (x + m) y m
   | otherwise = addMod (x - m) y m
+
+reverseWords :: String -> String
+reverseWords text = rev text "" ""
+  where
+    rev [] word result = putWord word result
+    rev (' ' : xs) word result = rev xs "" (putWord word result)
+    rev (x : xs) word result = rev xs (word ++ [x]) result
+
+    putWord "" result = result
+    putWord word "" = word
+    putWord word result = word ++ " " ++ result
