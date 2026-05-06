f676568527b71df8406749eca71778480380b854
topoff0 <vikonotopoff@gmail.com>
2026-05-06 20:30:39 +0300

    [pr05_1] Fix addMod function (now work with negative sum)

--- practice5/qclab/src/Lib.hs
+++ practice5/qclab/src/Lib.hs
@@ -6,5 +6,6 @@ where
 addMod :: Int -> Int -> Int -> Int
 addMod x y m
   | m <= 0 = error "Модуль должен быть положительным"
-  | (x + y) < m = x + y
+  | (x + y) < m && (x + y) > 0 = x + y
+  | (x + y) < 0 = addMod (x + m) y m
   | otherwise = addMod (x - m) y m
